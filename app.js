32954798f44d99557cf4f9e6c09018936dc539e2
David Sorm <david.sorm@prusa3d.cz>
2024-04-22 04:30:44 +0200

    remove unnecessary stuff

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ var activityReportRouter = require('./routes/activityreport');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
